1f60119bfa48f8cd374fd316796fa0276f68871c
Minchang Sung <tjdalsckd@gmail.com>
2021-06-24 21:36:30 +0900

    dd

--- load_robot.py
+++ load_robot.py
@@ -9,14 +9,62 @@ import tform as tf
 
 robot = InvertedPendulum();
 robot.torqueControllModeEnableForAll()
-while(1):
 
+prev_dq =[0,0,0]
+dt = 1/240.0
+u = [0,0,0]
+kpCartId = p.addUserDebugParameter("kpCart", 0, 1300, 1000)
+kdCartId = p.addUserDebugParameter("kdCart", 0, 1000, 200)
+kiCartId = p.addUserDebugParameter("kiCart", 0, 10000, 0)
+
+kpPoleId = p.addUserDebugParameter("kpPole", 0, 1300, 1000)
+kdPoleId = p.addUserDebugParameter("kdPole", 0, 1000, 200)
+kiPoleId = p.addUserDebugParameter("kiPole", 0, 10000, 0)
+
+#qd_=robot.IKinSpace(np.array([[0 ,0 ,1,0.7],[0 ,1,0,0],[-1,0 ,0,0.8],[0 ,0 ,0,1]]),np.array([0,0,0]))
+#qd_=qd_[0]
+#qd = np.array([qd_[0],qd_[1],qd_[2]])
+qd = np.array([0,0,0])
+dqd = np.array([0,0,0.0])
+ddqd = np.array([0,0,0])
+sum_e=0;
+sum_u0=0;
+while(1):
+	kpCart = p.readUserDebugParameter(kpCartId)
+	kdCart = p.readUserDebugParameter(kdCartId)
+	kiCart = p.readUserDebugParameter(kiCartId)
+	kpPole = p.readUserDebugParameter(kpPoleId)
+	kdPole = p.readUserDebugParameter(kdPoleId)
+	kiPole = p.readUserDebugParameter(kiPoleId)
+	Kp = np.diag([kpCart,kpPole,kpPole])
+	Ki = np.diag([kiCart,kiPole,kiPole])
+	Kd = np.diag([kdCart,kdPole,kdPole])
+	
+	
+	
 	q=robot.getJointPositions();
 	dq=robot.getJointVelocities();
+	e=qd-q;
+	edot = dqd-dq;
+	sum_e = sum_e+e*dt;
+
+
+	T=robot.FKinSpace(q)
+	J=robot.JacobianSpace
+	M=robot.getMassMatrix(q)
+	G=robot.getGravityAndCoriolis(q,dq,[0,0,0])
 	
-	u = [0,0,0]
-	robot.setBaseVelocities([u[0]])		
+	p_term = Kp.dot(e - dq*dt)
+	d_term = Kd.dot(edot)
+	qddot = np.linalg.solve(a=(M + Kd * dt),b=p_term + d_term - G)
+	u = M.dot(ddqd+Kp.dot(e)+Ki.dot(sum_e)+Kd.dot(edot))+G
 	robot.setArmJointTorques([u[1],u[2]])
-	T=robot.forwardKinematics(q)
+	sum_u0=0
+	
+	#robot.setBaseVelocities([kpCart*e[0]+kdCart*edot[0]+G[0]])			
+	robot.setBaseJointTorques([u[0]])
+	print("error",e)
+	print("u",u)
 	
 	robot.oneStep();
+
--- robot.py
+++ robot.py
@@ -7,10 +7,13 @@ import scipy.linalg as la
 import modern_robotics as mr
 class Robot:
     def __init__(self, robotPATH, startPosition, startOrientation, maxForce, controlMode=pb.POSITION_CONTROL, planePATH="plane.urdf",timeStep=1/240.0):
-        physicsClient = pb.connect(pb.GUI)
+        try:
+            physicsClient = pb.connect(pb.GUI)
+        except:
+            pass
         pb.setAdditionalSearchPath(pybullet_data.getDataPath())
         pb.setGravity(0,0,-9.806)
-        self._planeId = pb.loadURDF(planePATH)
+        #self._planeId = pb.loadURDF(planePATH)
         self._robotId = pb.loadURDF(robotPATH,startPosition, pb.getQuaternionFromEuler(startOrientation))
         self._controlMode = controlMode
         self.numJoint = pb.getNumJoints(self._robotId)
@@ -182,10 +185,10 @@ class InvertedPendulum(Robot):
 		self._maxForce = maxForce;
 		self._Kp = [500,500,500];
 		self._Kd = [150,150,150];
+		self._timeStep=timeStep
 		W1 = 0.2
 		H1 = 0.05
 		H2 = 0.25
-		
 		L1 = 0.5;
 		L2 = 0.5;
 		
@@ -193,34 +196,86 @@ class InvertedPendulum(Robot):
 				[0,1,0,0],
 				[0,0,1,L1+L2+H1+H2],
 				[0,0,0,1]])
+		M01=  np.array([[1, 0, 0,        0],
+				[0, 1, 0,        0],
+				[0, 0, 1,        0],
+				[0, 0, 0,        1]])
+		M12=  np.array([[1, 0, 0,        W1],
+				[0, 1, 0,        0],
+				[0, 0, 1, H1+H2+L1/2],
+				[0, 0, 0,        1]])
+		M23=  np.array([[1, 0, 0,        0],
+				[0, 1, 0,        0],
+				[0, 0, 1, L1/2+L2/2],
+				[0, 0, 0,        1]])
+		M34=  np.array([[1, 0, 0,        0],
+				[0, 1, 0,        0],
+				[0, 0, 1, L2/2],
+				[0, 0, 0,        1]])
+		G1 = np.diag([1,1,1,10*9.8,10*9.8,10*9.8])
+		G2 = np.diag([1,1,1,2*9.8,2*9.8,2*9.8])
+		G3 = np.diag([1,1,1,2*9.8,2*9.8,2*9.8])	
+		self.Glist=np.array([G1,G2,G3])
+		self.Mlist=np.array([M01,M12,M23,M34])
+		self.g = np.array([0,0,-9.8])
+		print(np.matmul(np.matmul(np.matmul(M01,M12),M23),M34))
 		self.Slist = np.array([[0,0,0,1,0,0],[0,1,0,-(H1+H2),0,W1],[0,1,0,-(H1+H2+L1),0,W1]]).T
 	def torqueControllModeEnableForAll(self):
 		pb.setJointMotorControlArray(self._robotId, jointIndices=self._jointList, controlMode=pb.VELOCITY_CONTROL, forces=[0]*len(self._jointList))
 		self._controlMode = pb.TORQUE_CONTROL
-	def inverseKinematics(self, p_ref, omega_ref):               
-		jointPositions=[0,0,0]
+	def IKinSpace(self, T,jointPositions0,eomg=0.01,ev=0.001):               
+		jointPositions=mr.IKinSpace(self.Slist,self.M,T,jointPositions0,eomg,ev)
 		return jointPositions
-	def forwardKinematics(self, jointPositions):
+	def FKinSpace(self, jointPositions):
 		thetalist = np.array([jointPositions[0],jointPositions[1],jointPositions[2]])		
 		T_eef = mr.FKinSpace(self.M,self.Slist,thetalist)
 		return T_eef
+	def JacobianSpace(self, jointPositions):
+		thetalist = np.array([jointPositions[0],jointPositions[1],jointPositions[2]])		
+		J = mr.JacobianSpace(self.Slist,thetalist)
+		return J
 	def inverseDynamics(self, jointPositions,jointVelocities,targetAccelerations):
 		torque = [0,0,0]
 		return torque 
+	def ForwardDynamics(self,jointPositions,jointVelocities,torques,Ftip):
+		thetalist = np.array([jointPositions[0],jointPositions[1],jointPositions[2]])	
+		dthetalist = np.array([jointVelocities[0],jointVelocities[1],jointVelocities[2]])
+		taulist = np.array([torques[0],torques[1],torques[2]])
+		Ftip_ = np.array([Ftip[0],Ftip[1],Ftip[2],Ftip[3],Ftip[4],Ftip[5]])
+		jointAccelrations = mr.ForwardDynamics(thetalist,dthetalist,taulist,self.g,Ftip.T,self.Mlist,self.Glist,self.Slist)
+		return jointAccelrations
+	def EulerStep(self,thetalist,dthetalist,ddthetalist,dt,intRes):
+		return mr.EulerStep(thetalist, dthetalist,ddthetalist, 1.0 * dt / intRes)		
 	def getJointPositions(self):
 		jointStates = pb.getJointStates(self._robotId, jointIndices=self._jointList)
 		jointPositions = [jointStates[0][0],jointStates[1][0],jointStates[2][0]]
-		return jointPositions 		
+		return np.array(jointPositions)
 	def getJointVelocities(self):
 		jointStates = pb.getJointStates(self._robotId, jointIndices=self._jointList)
 		jointVelocities = [jointStates[0][1],jointStates[1][1],jointStates[2][1]]	
-		return jointVelocities		
+		return np.array(jointVelocities)
 	def setEnableFTsensor(self):
 		pb.enableJointForceTorqueSensor(self._robotId, jointIndex=self._FTsensorJoint)
+	def setJointState(self,jointPositions):
+		pb.resetJointState(self._robotId,0,jointPositions[0])		
+		pb.resetJointState(self._robotId,3,jointPositions[1])		
+		pb.resetJointState(self._robotId,4,jointPositions[2])		
+		
 	def getFTsensorValues(self):
 		val=pb.getJointState(self._robotId, jointIndex=self._FTsensorJoint)
 		FTsensorValues =val[2];
 		return FTsensorValues
+	def getMassMatrix(self,jointPositions):
+		q = [jointPositions[0],jointPositions[1],jointPositions[2]]
+		M=pb.calculateMassMatrix(self._robotId,q)
+		return np.array(M)
+	def getGravityAndCoriolis(self,jointPositions,jointVelocities,targetAccelerations):
+		q = [jointPositions[0],jointPositions[1],jointPositions[2]]
+		dq = [jointVelocities[0],jointVelocities[1],jointVelocities[2]]
+		target_ddq = [targetAccelerations[0],targetAccelerations[1],targetAccelerations[2]]
+		G = pb.calculateInverseDynamics(self._robotId, q, dq, target_ddq)
+		return np.array(G)
+				
 	def getLinkState(self,linkIndex):
 		val=pb.getLinkState(self._robotId, linkIndex=linkIndex)
 		return val[0]
@@ -231,6 +286,11 @@ class InvertedPendulum(Robot):
 			n = n+1;
 	def setBaseVelocities(self,targetVels):
 		pb.setJointMotorControlArray(self._robotId,self._basejointList, controlMode=pb.VELOCITY_CONTROL,targetVelocities = targetVels,forces=[self._maxForce]*len(self._basejointList))
+	def setBaseJointTorques(self,taus):
+		n = 0;
+		for j  in self._basejointList:
+			pb.setJointMotorControlMultiDof(self._robotId,j, controlMode=pb.TORQUE_CONTROL,force=[taus[n]])
+			n = n+1;	
 	def printDebugLine(self,Tstart,Tend):	 
 		pb.addUserDebugLine([Tstart[0,3],Tstart[1,3],Tstart[2,3]],[Tend[0,3],Tend[1,3],Tend[2,3]],lineColorRGB=[1,0,0])
  		
